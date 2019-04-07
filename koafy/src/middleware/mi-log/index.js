df0d59f2f7b46b5a92721a0b3fd541d226dc2cec
moofyu <moofyu@163.com>
2019-04-07 11:58:21 +0800

    koafy

--- koafy/src/middleware/mi-log/index.js
+++ koafy/src/middleware/mi-log/index.js
@@ -1,5 +1,5 @@
 const log4js = require('log4js')
-const config = require('config').log4js
+const config = require('@coofy/config').log4js
 const methods = ['trace', 'debug', 'info', 'warn', 'error', 'fatal', 'mark']
 const access = require('./access')
 
