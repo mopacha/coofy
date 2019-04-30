194f5ca1daa4371064c656bdc223cf978da66293
unknown <wangpeng04@ppdai.com>
2019-04-30 19:13:22 +0800

    add

--- resource-optimizer/tasks/pkNode.js
+++ resource-optimizer/tasks/pkNode.js
@@ -10,7 +10,7 @@ module.exports = config => {
 
 	gulp.task('copyMudules', function() {
 		return gulp
-			.src(['node_modules/**/**', '!node_modules/*@babel*/**', '!node_modules/*gulp*/**'], {
+			.src(['node_modules/**/**', '!node_modules/*gulp*/**'], {
 				base: '.'
 			})
 			.pipe(gulp.dest(REV))
