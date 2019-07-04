8904adc5a76f991f74d13baa09a0a78c83840d9d
ZFY <wangpeng04@ppdai.com>
2019-07-03 23:26:31 -0700

    for pc

---
+++ h5-adapt/README.md
@@ -0,0 +1,5 @@
+
+
+## rem adapt 
+
+### @2.0.0  for pc 
--- h5-adapt/index.js
+++ h5-adapt/index.js
@@ -1,21 +1,40 @@
-;(function(win, doc) {
+; (function (win, doc) {
 	var docEl = doc.documentElement,
 		dpr = win.devicePixelRatio || 1,
 		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-		recalc = function() {
+		recalc = function () {
 			var clientWidth = docEl.clientWidth,
 				rem = clientWidth < 500 ? clientWidth / 10 : 50
 			docEl.style.fontSize = rem + 'px'
+			setPcMaxWidth()
 		},
-		setBodyFontSize = function() {
+		setBodyFontSize = function () {
 			if (document.body) {
-				document.body.style.fontSize = 12 *(dpr)  + 'px'
+				document.body.style.fontSize = 12 * (dpr) + 'px'
 			} else {
 				document.addEventListener('DOMContentLoaded', setBodyFontSize)
 			}
 		}
-	setBodyFontSize()
 
+	isPC = function () {
+		var userAgentInfo = navigator.userAgent;
+		var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");
+		var flag = true;
+		for (var v = 0; v < Agents.length; v++) {
+			if (userAgentInfo.indexOf(Agents[v]) > 0) { flag = false; break; }
+		}
+		return flag;
+	}
+	setPcMaxWidth = function () {
+		if (isPC()) {
+			if (document.body) {
+				document.body.style.maxWidth = 500 + 'px'
+				document.body.style.margin = '0px auto'
+			}
+		}
+	}
+	setBodyFontSize()
 	win.addEventListener(resizeEvt, recalc)
 	doc.addEventListener('DOMContentLoaded', recalc, false)
+
 })(window, document)
